df01a6b7ff57158802e4d189bc0170fb971178b6
Vinnie <vincent.king1224@gmail.com>
2017-08-14 00:52:28 -0700

    grab id functionality

--- love-find.js
+++ love-find.js
@@ -3,25 +3,20 @@ var jsdom = require("jsdom/lib/old-api.js");
 const express = require('express');
 const headers = require('./private');
 const fs = require('fs');
+const idData = require('./data');
 
 let users = [];
 let userLovers = {};
 let test = ['rosy126', 'sneedsi', 'mdoucette1'];
+let promises = [];
 
-
-  test.forEach(item => {
-    checkLovePage(item);
+ async function getLoversFromUsers(users) {
+  users.forEach(async item => {
+    promises.push(checkLovePagePromise(item));
   });
-
-  setTimeout(() => {
-    console.log(userLovers);
-  },6000)
-
-
-
-
-
-
+  await Promise.all(promises);
+  fs.writeFileSync('./data.json', JSON.stringify(userLovers, null, 2) , 'utf-8');
+}
 
 function getUsers() {
   jsdom.env( {
@@ -34,32 +29,39 @@ function getUsers() {
         users.push(elems[i].innerHTML);
       }
       console.log(users);
+      getLoversFromUsers(users);
     }
   })
 }
 
-
-  function checkLovePage(user) {
-    jsdom.env( {
-      url:`https://poshmark.com/closet/${user}/love-notes`,
-      headers: headers.scrapeHeader,
-      scripts:["http://code.jquery.com/jquery.js"],
-      done: function (err, window) {
-        const elems = window.document.getElementsByClassName('text-con');
-        let lovers = [];
-        let loveNote;
-        for (let el in elems) {
-          loveNote = elems[el].childNodes[0].innerHTML
-          if (user !== loveNote) {
-            lovers.push(loveNote);
+  function checkLovePagePromise(user) {
+    return new Promise ((resolve, reject) => {
+      jsdom.env( {
+        url:`https://poshmark.com/closet/${user}/love-notes`,
+        headers: headers.scrapeHeader,
+        scripts:["http://code.jquery.com/jquery.js"],
+        done: function (err, window) {
+          if (err) {
+            reject(err);
           }
+          const elems = window.document.getElementsByClassName('user-image s');
+          let lovers = [];
+          let loveId;
+          for (let el in elems) {
+            loveId = elems[el].src;
+            if (grabId(loveId)) {
+              lovers.push(grabId(loveId))
+            }
+          }
+          userLovers[user] = lovers;
+          resolve();
         }
-        userLovers[user] = lovers;
-      }
+      })
     })
+
   }
 
-  //checkLovePage('veronicaliz');
+  //checkLovePagePromise('sfvhotmess');
 
  
 function callback(error, response, body) {
@@ -77,8 +79,23 @@ function follow(id) {
   return request(options, callback)
 }
 
-//let obj = {data: lovers}; 
+function grabId(str) {
+  if (!str || str.includes('facebook') || str.includes('user-default')) {
+    return false;
+  }
+  let newStr = str.split('users');
+  newStr = newStr[1].split('');
+  for (let i = 0; i < 12; i++) {
+    newStr.shift();
+  }
+  let id = [];
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] === '/') {
+      break;
+    } else {
+      id.push(newStr[i]) 
+    }
 
-//fs.writeFileSync('./data.json', JSON.stringify(obj, null, 2) , 'utf-8');
-
-
+  }
+  return id.join('')
+}
